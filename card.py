68cc58c1a5c71c5ca1f24c36064a54d6e3e8061c
jwatts82 <jessperr182@hotmail.com>
2021-02-27 20:48:49 -0800

    player class

--- card.py
+++ card.py
@@ -26,4 +26,22 @@ class Deck:
     def deal_one(self):
         return self.all_cards.pop()
 
+class Player:
+    def __init_(self,name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([])
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    
+    def__str__(self):
+    retur f'Player {self.name} ahs {len(self.all_cards)} cards.'
+
+
 
